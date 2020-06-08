7d74d4928a0e7e14b91d2572ebe9cbd512cc1456
garnment <david-gautier44@orange.fr>
2020-06-08 11:00:20 +0200

    Intégration + JS

--- src/static/dist/js/script.js
+++ src/static/dist/js/script.js
@@ -1,1 +1,47 @@
-"use strict";function _typeof(r){return(_typeof="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(r){return typeof r}:function(r){return r&&"function"==typeof Symbol&&r.constructor===Symbol&&r!==Symbol.prototype?"symbol":typeof r})(r)}!function(t){var e={};function o(r){if(e[r])return e[r].exports;var n=e[r]={i:r,l:!1,exports:{}};return t[r].call(n.exports,n,n.exports,o),n.l=!0,n.exports}o.m=t,o.c=e,o.d=function(r,n,t){o.o(r,n)||Object.defineProperty(r,n,{enumerable:!0,get:t})},o.r=function(r){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(r,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(r,"__esModule",{value:!0})},o.t=function(n,r){if(1&r&&(n=o(n)),8&r)return n;if(4&r&&"object"===_typeof(n)&&n&&n.__esModule)return n;var t=Object.create(null);if(o.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:n}),2&r&&"string"!=typeof n)for(var e in n)o.d(t,e,function(r){return n[r]}.bind(null,e));return t},o.n=function(r){var n=r&&r.__esModule?function(){return r.default}:function(){return r};return o.d(n,"a",n),n},o.o=function(r,n){return Object.prototype.hasOwnProperty.call(r,n)},o.p="",o(o.s="./src/static/js/script.js")}({"./src/static/js/script.js":function srcStaticJsScriptJs(module,exports){eval('(function ( $ ) {\r\n\r\n    $.scrolling = function(  ) {\r\n\r\n        $( window ).scroll( function () {\r\n            var scrollTop = $( window ).scrollTop();\r\n            var mainNavHeight = $( "#mainNav" ).height();\r\n\r\n            // Ombrage sur la navbar\r\n            if (scrollTop > mainNavHeight) {\r\n                $( "#mainNav" ).addClass(\'scrolling\');\r\n            } else {\r\n                $( "#mainNav" ).removeClass(\'scrolling\');\r\n            }\r\n\r\n            \r\n        });\r\n\r\n    };\r\n\r\n}( jQuery ));\r\n\r\n\r\n$(document).ready(function(){\r\n    $.scrolling();\r\n});\r\n\r\n\n\n//# sourceURL=webpack:///./src/static/js/script.js?')}});
+"use strict";
+function _typeof(n) {
+    return (_typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (n) {
+        return typeof n
+    } : function (n) {
+        return n && "function" == typeof Symbol && n.constructor === Symbol && n !== Symbol.prototype ? "symbol" : typeof n
+    })(n)
+}
+!function (i) {
+    var t = {};
+
+    function e(n) {
+        if (t[n])return t[n].exports;
+        var r = t[n] = {i: n, l: !1, exports: {}};
+        return i[n].call(r.exports, r, r.exports, e), r.l = !0, r.exports
+    }
+
+    e.m = i, e.c = t, e.d = function (n, r, i) {
+        e.o(n, r) || Object.defineProperty(n, r, {enumerable: !0, get: i})
+    }, e.r = function (n) {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(n, Symbol.toStringTag, {value: "Module"}), Object.defineProperty(n, "__esModule", {value: !0})
+    }, e.t = function (r, n) {
+        if (1 & n && (r = e(r)), 8 & n)return r;
+        if (4 & n && "object" === _typeof(r) && r && r.__esModule)return r;
+        var i = Object.create(null);
+        if (e.r(i), Object.defineProperty(i, "default", {
+                enumerable: !0,
+                value: r
+            }), 2 & n && "string" != typeof r)for (var t in r)e.d(i, t, function (n) {
+            return r[n]
+        }.bind(null, t));
+        return i
+    }, e.n = function (n) {
+        var r = n && n.__esModule ? function () {
+            return n.default
+        } : function () {
+            return n
+        };
+        return e.d(r, "a", r), r
+    }, e.o = function (n, r) {
+        return Object.prototype.hasOwnProperty.call(n, r)
+    }, e.p = "", e(e.s = "./src/static/js/script.js")
+}({
+    "./src/static/js/script.js": function srcStaticJsScriptJs(module, exports) {
+        eval('(function ($) {\r\n\r\n    var mainNavHeight;\r\n\r\n    var r1;\r\n    var ro1;\r\n    var rod1;\r\n    var ro1Width;\r\n\r\n    var r3;\r\n    var ro3;\r\n    var rod3;\r\n    var ro3Width;\r\n\r\n    /** Create autoslide */\r\n    var autoSlide = null;\r\n\r\n    $.createAutoSlide = function () {\r\n        autoSlide = setInterval(function(){\r\n            $.slide();\r\n        }, 3000 );\r\n    }\r\n\r\n    $.deleteAutoSlide = function () {\r\n        clearInterval(autoSlide);\r\n        autoSlide = null;\r\n    }\r\n\r\n\r\n    /** Loading images */\r\n    $.fn.imgLoad = function(callback) {\r\n        return this.each(function() {\r\n            if (callback) {\r\n                if (this.complete || $(this).height() > 0) {\r\n                    callback.apply(this);\r\n                }\r\n                else {\r\n                    $(this).on(\'load\', function(){\r\n                        callback.apply(this);\r\n                    });\r\n                }\r\n            }\r\n        });\r\n    };\r\n\r\n    /** SCROLLING */\r\n    $.scrolling = function () {\r\n\r\n        $(window).scroll(function () {\r\n            var scrollTop = $(window).scrollTop();\r\n\r\n            // Ombrages sur la navbar et le container\r\n            if (scrollTop > mainNavHeight) {\r\n                $("#mainNav").addClass(\'scrolling\');\r\n                $("#mainContainer").addClass(\'scrolling\');\r\n            } else {\r\n                $("#mainNav").removeClass(\'scrolling\');\r\n                $("#mainContainer").removeClass(\'scrolling\');\r\n            }\r\n\r\n            // Parallaxe\r\n            var width1 = parseInt(ro1Width + (scrollTop/7));\r\n            var width3 = parseInt(scrollTop/10);\r\n\r\n            rod1.css({width: width1});\r\n            rod3.css({top: -width3}).css({left: -width3});\r\n        });\r\n    };\r\n\r\n\r\n\r\n\r\n\r\n    /** Init scrolling */\r\n    $.initScrolling = function () {\r\n\r\n        var nbImg = $("#ambiances img").length;\r\n        var nbLoaded = 0;\r\n\r\n        $("#ambiances img").imgLoad(function(){\r\n            nbLoaded++;\r\n            $( this ).fadeTo( "fast" , 1);\r\n\r\n            if (nbLoaded === nbImg) {\r\n                 mainNavHeight = $("#mainNav").height();\r\n                 r1 = $(\'#room1\');\r\n                 ro1 = $(\'#room1 .overflow\');\r\n                 rod1 = $(\'#room1 .overflow div\');\r\n                 ro1Width =  parseInt(ro1.width());\r\n\r\n                 r3 = $(\'#room3\');\r\n                 ro3 = $(\'#room3 .overflow\');\r\n                 rod3 = $(\'#room3 .overflow div\');\r\n                 ro3Width = parseInt(r3.width());\r\n\r\n                 ro1.css({width: ro1Width});\r\n                 ro1.css({height: ro1.height()});\r\n\r\n                 ro3.css({width: ro3Width});\r\n                 ro3.css({height: ro3.height()});\r\n\r\n                $.scrolling();\r\n            }\r\n\r\n        });\r\n\r\n    }\r\n\r\n\r\n    \r\n    /** Slideshow */\r\n    $.slideShow = function() {\r\n\r\n        $("#btSlideContainer #prev").bind(\'click\', function(){\r\n            $.deleteAutoSlide();\r\n            $.slide(this);\r\n        });\r\n\r\n        $("#btSlideContainer #next").bind(\'click\', function () {\r\n            $.deleteAutoSlide();\r\n            $.slide(this);\r\n        });\r\n\r\n        /** Lancement auto par défaut */\r\n        $.createAutoSlide();\r\n\r\n    }\r\n\r\n\r\n    $.slide = function(bt=\'next\'){\r\n        var width = $("#imgSlide :first").width();\r\n        var imgSlide = $("#imgSlide");\r\n        var textSlide = $(\'#textSlide\')\r\n        textSlide.fadeOut(150);\r\n\r\n\r\n        if (bt.id === \'prev\') {\r\n            imgSlide.animate({marginLeft: -width}, 800, function () {\r\n                $(this).css({marginLeft: 0}).find("div:last").after($(this).find("div:first"));\r\n                textSlide.fadeIn(300);\r\n            })\r\n        } else {\r\n            imgSlide.css({marginLeft: -width}).find("div:first").before((imgSlide).find("div:last"));\r\n            imgSlide.animate({marginLeft: 0}, 800, function () {\r\n                textSlide.fadeIn(300);\r\n            });\r\n        }\r\n\r\n    }\r\n\r\n\r\n\r\n    /** Loading slideshow SLIDESHOW */\r\n    var nbSlides = $("#imgSlide img").length;\r\n    var nbLoaded = 0;\r\n\r\n    $.loadingSlide = function() {\r\n        $("#imgSlide img").imgLoad(function(){\r\n            nbLoaded++;\r\n            if (nbLoaded == nbSlides) {\r\n                $("#loader").hide();\r\n                $("#slideContainer").removeClass(\'loading\');\r\n                $.slideShow();\r\n            }\r\n        });\r\n    };\r\n\r\n\r\n\r\n\r\n}(jQuery));\r\n\r\n\r\n$(document).ready(function () {\r\n    $.loadingSlide();\r\n    $.initScrolling();\r\n});\r\n\r\n\n\n//# sourceURL=webpack:///./src/static/js/script.js?')
+    }
+});
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,27 +1,171 @@
 (function ($) {
 
+    var mainNavHeight;
+
+    var r1;
+    var ro1;
+    var rod1;
+    var ro1Width;
+
+    var r3;
+    var ro3;
+    var rod3;
+    var ro3Width;
+
+    /** Create autoslide */
+    var autoSlide = null;
+
+    $.createAutoSlide = function () {
+        autoSlide = setInterval(function () {
+            $.slide();
+        }, 3000);
+    };
+
+    $.deleteAutoSlide = function () {
+        clearInterval(autoSlide);
+        autoSlide = null;
+    };
+
+
+    /** Loading images */
+    $.fn.imgLoad = function (callback) {
+        return this.each(function () {
+            if (callback) {
+                if (this.complete || $(this).height() > 0) {
+                    callback.apply(this);
+                }
+                else {
+                    $(this).on('load', function () {
+                        callback.apply(this);
+                    });
+                }
+            }
+        });
+    };
+
+    /** SCROLLING */
     $.scrolling = function () {
 
         $(window).scroll(function () {
             var scrollTop = $(window).scrollTop();
-            var mainNavHeight = $("#mainNav").height();
 
-            // Ombrage sur la navbar
+            // Ombrages sur la navbar et le container
             if (scrollTop > mainNavHeight) {
                 $("#mainNav").addClass('scrolling');
+                $("#mainContainer").addClass('scrolling');
             } else {
                 $("#mainNav").removeClass('scrolling');
+                $("#mainContainer").removeClass('scrolling');
             }
 
+            // Parallaxe
+            var width1 = parseInt(ro1Width + (scrollTop / 7));
+            var width3 = parseInt(scrollTop / 10);
+
+            rod1.css({width: width1});
+            rod3.css({top: -width3}).css({left: -width3});
+        });
+    };
+
+
+    /** Init scrolling */
+    $.initScrolling = function () {
+
+        var nbImg = $("#ambiances img").length;
+        var nbLoaded = 0;
+
+        $("#ambiances img").imgLoad(function () {
+            nbLoaded++;
+            $(this).fadeTo("fast", 1);
+
+            if (nbLoaded === nbImg) {
+                mainNavHeight = $("#mainNav").height();
+                r1 = $('#room1');
+                ro1 = $('#room1 .overflow');
+                rod1 = $('#room1 .overflow div');
+                ro1Width = parseInt(ro1.width());
+
+                r3 = $('#room3');
+                ro3 = $('#room3 .overflow');
+                rod3 = $('#room3 .overflow div');
+                ro3Width = parseInt(r3.width());
+
+                ro1.css({width: ro1Width});
+                ro1.css({height: ro1.height()});
+
+                ro3.css({width: ro3Width});
+                ro3.css({height: ro3.height()});
+
+                $.scrolling();
+            }
 
         });
 
     };
 
+
+    /** Slideshow */
+    $.slideShow = function () {
+
+        $("#btSlideContainer #prev").bind('click', function () {
+            $.deleteAutoSlide();
+            $.slide(this);
+        });
+
+        $("#btSlideContainer #next").bind('click', function () {
+            $.deleteAutoSlide();
+            $.slide(this);
+        });
+
+        /** Lancement auto par défaut */
+        $.createAutoSlide();
+
+    };
+
+
+    $.slide = function (bt = 'next') {
+        var width = $("#imgSlide :first").width();
+        var imgSlide = $("#imgSlide");
+        var textSlide = $('#textSlide');
+        textSlide.fadeOut(150);
+
+
+        if (bt.id === 'prev') {
+            imgSlide.animate({marginLeft: -width}, 800, function () {
+                $(this).css({marginLeft: 0}).find("div:last").after($(this).find("div:first"));
+                textSlide.fadeIn(300);
+            })
+        } else {
+            imgSlide.css({marginLeft: -width}).find("div:first").before((imgSlide).find("div:last"));
+            imgSlide.animate({marginLeft: 0}, 800, function () {
+                textSlide.fadeIn(300);
+            });
+        }
+
+    };
+
+
+    /** Loading slideshow SLIDESHOW */
+    var nbSlides = $("#imgSlide img").length;
+    var nbLoaded = 0;
+
+    $.loadingSlide = function () {
+        $("#imgSlide img").imgLoad(function () {
+            nbLoaded++;
+            if (nbLoaded == nbSlides) {
+                $("#loader").hide();
+                $("#slideContainer").removeClass('loading');
+                $.slideShow();
+            }
+        });
+    };
+
+
 }(jQuery));
 
 
 $(document).ready(function () {
-    $.scrolling();
+    $.loadingSlide();
+    $.initScrolling();
 });
 
